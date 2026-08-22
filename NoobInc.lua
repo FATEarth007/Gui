8338cb4c69d1337893f779b07d30425dcfe3232c
FATEarth <121425603+FATEarth007@users.noreply.github.com>
2026-08-22 16:55:15 -0400

    Update NoobInc.lua

--- NoobInc.lua
+++ NoobInc.lua
@@ -788,3 +788,27 @@ if TianMarkPress then
 	)
 
 end
+
+
+
+-- ==========================================
+-- Anti AFK
+-- ==========================================
+
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local VirtualUser = game:GetService("VirtualUser")
+
+local currentCamera = game.Workspace.CurrentCamera
+
+player.Idled:Connect(function()
+	VirtualUser:Button2Down(Vector2.zero, currentCamera.CFrame)
+	task.wait(1)
+	VirtualUser:Button2Up(Vector2.zero, currentCamera.CFrame)
+
+	print("Player Successfully UnIdled.")
+end)
+
+print("Script Ran")
